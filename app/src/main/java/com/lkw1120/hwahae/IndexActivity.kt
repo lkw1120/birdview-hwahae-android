e1a3e5cb0e3927eb6532bd2890567dc893f82063
kang-won Lee <lkwkang@gmail.com>
2020-01-09 16:58:29 +0900

    과제 시작

---
+++ app/src/main/java/com/lkw1120/hwahae/IndexActivity.kt
@@ -0,0 +1,12 @@
+package com.lkw1120.hwahae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IndexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+}
